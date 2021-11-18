0f06f45526a49764b0b3309f03c420ab60f20010
Kristian <kristian.zh.apostolov@gmail.com>
2021-11-18 20:11:15 +0000

    Add index.html, about.html and routing

--- main.js
+++ main.js
@@ -1,7 +1,35 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => { // Callback called every time a request to the server is sent
-    console.log('request made');
+    console.log(req.url, req.method);
+    
+    // set header content type
+    res.setHeader('Content-Type', 'text/html');
+    
+    let path = './views/';
+    res.statusCode = 200;
+    switch (req.url) {
+        case '/':
+            path += 'index.html';
+            break;
+        case '/about':
+            path += 'about.html';
+            break;
+        default:
+            res.statusCode = 404;
+    }
+
+
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
+            // res.write(data);
+            res.end(data); // When sending one thing data can be passed to end method
+        } 
+    });
 });
 
 const port = 3000;
