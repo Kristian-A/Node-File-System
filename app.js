f0506f2181e704398d06dc57468603bc19a7a5bd
Kristian <kristian.zh.apostolov@gmail.com>
2021-11-18 22:32:16 +0000

    Add .ejs views

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000);
+app.set('view engine', 'ejs');
+
+
+app.use((req, res, next) => {
+    console.log('Request made');
+    console.log('hostname: ' + req.hostname);
+    console.log('path: ' + req.path); 
+    next();
+});
+
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', {root: __dirname});
+    const items = [
+        {title: 'first', value: 'wow'},
+        {title: 'second', value: 'this'},
+        {title: 'third', value: 'works'},
+    ];
+
+    res.render('index', {title: 'Home', items});    
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', {root: __dirname});
+    res.render('about');    
 });
+
+app.get('/item/create', (req, res) => {
+    res.render('create');
+});
+
+app.use((req, res) => {
+    res.status(404).send('Page not found!');
+});
+
+app.listen(3000);
