7c121c7136221b27c87acbd3af9e49c8f9b79da8
Kristian <kristian.zh.apostolov@gmail.com>
2021-11-18 16:21:04 +0000

    Add stream tests

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
+const fs = require('fs');
 
-console.log(window);
+const rs = fs.createReadStream('./files/text1.txt', {encoding: 'utf8'});
+const ws = fs.createWriteStream('./files/text2.txt');
+let text = ''
+
+
+// One way to transfer data from one file to another
+// readStream.on('data', chunk => {
+//     ws.write(chunk);
+// });
+
+// Piping
+rs.pipe(ws);
