031056d4680a420da79fb4591ddf07916813048e
Kristian <kristian.zh.apostolov@gmail.com>
2021-11-19 11:31:35 +0000

    Add file model + create and update

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const { nextTick } = require('process');
+const Files = require('./models/File.js');
 const {dbname, username, password} = require('./db-connection.js');
+const { redirect } = require('statuses');
 
 const dbURI = `mongodb+srv://${username}:${password}@nodefilesystem.tp1w8.mongodb.net/${dbname}?retryWrites=true&w=majority`
 
@@ -20,10 +21,22 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.render('index');
+    Files.find()
+        .then(files => res.render('index', {files}))
+        .catch(err => console.log(err));
 });
 
-app.post('/', (req, res, next) => {
-    
+app.post('/', (req, res) => {
+    new Files(req.body).save();
     res.redirect('/');
+});
+
+app.post('/delete/:id', (req, res) => {
+    Files.findByIdAndDelete(req.params.id)
+        .then(result => res.redirect('/'))
+        .catch(err => console.log(err));
+});
+
+app.use((req, res) => {
+    res.status(404).send('Page not found!');
 })
